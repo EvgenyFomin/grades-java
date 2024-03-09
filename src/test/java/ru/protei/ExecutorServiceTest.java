be4a72d5d201e9bd60df541de60d801cf44d4a35
fomin_e <fomin_e@protei-lab.ru>
2024-03-09 09:51:18 +0300

    executor-service - add scheduled thread pool tests

--- src/test/java/ru/protei/ExecutorServiceTest.java
+++ src/test/java/ru/protei/ExecutorServiceTest.java
@@ -44,12 +44,12 @@ public class ExecutorServiceTest {
     public void testCachedThreadPoolAsync() {
         ExecutorService executorService = Executors.newCachedThreadPool();
         Arrays.asList(
-                executorService.submit(this::sleep),
-                executorService.submit(this::sleep),
-                executorService.submit(this::sleep),
-                executorService.submit(this::sleep),
-                executorService.submit(this::sleep),
-                executorService.submit(this::sleep)
+                executorService.submit(this::sleep1000),
+                executorService.submit(this::sleep1000),
+                executorService.submit(this::sleep1000),
+                executorService.submit(this::sleep1000),
+                executorService.submit(this::sleep1000),
+                executorService.submit(this::sleep1000)
         ).forEach(this::await);
     }
 
@@ -57,13 +57,57 @@ public class ExecutorServiceTest {
     public void testSingleThreadPoolSync() {
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         Arrays.asList(
-                executorService.submit(this::sleep),
-                executorService.submit(this::sleep),
-                executorService.submit(this::sleep),
-                executorService.submit(this::sleep),
-                executorService.submit(this::sleep),
-                executorService.submit(this::sleep)
+                executorService.submit(this::sleep1000),
+                executorService.submit(this::sleep1000),
+                executorService.submit(this::sleep1000),
+                executorService.submit(this::sleep1000),
+                executorService.submit(this::sleep1000),
+                executorService.submit(this::sleep1000)
         ).forEach(this::await);
+    }
+
+    @Test
+    public void testScheduledThreadPoolSingle() {
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+        executorService.schedule(() -> System.out.println("hello once"), 1, TimeUnit.SECONDS);
+
+        sleep3000();
+    }
+
+    @Test
+    public void testScheduledThreadPoolRepeatWaitLessThanPeriod() {
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(10);
+        ScheduledExecutorService executorService2 = Executors.newScheduledThreadPool(10);
+
+        long start = System.currentTimeMillis();
+        executorService.scheduleAtFixedRate(() -> {
+            System.out.println("hello repeat: " + (System.currentTimeMillis() - start));
+            sleep1000();
+        }, 0, 2, TimeUnit.SECONDS);
+        executorService2.scheduleWithFixedDelay(() -> {
+            System.out.println("hello repeat, fixed start delay: " + (System.currentTimeMillis() - start));
+            sleep1000();
+        }, 0, 2, TimeUnit.SECONDS);
+
+        sleep20000();
+    }
+
+    @Test
+    public void testScheduledThreadPoolRepeatWaitMoreThanPeriod() {
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(10);
+        ScheduledExecutorService executorService2 = Executors.newScheduledThreadPool(10);
+
+        long start = System.currentTimeMillis();
+        executorService.scheduleAtFixedRate(() -> {
+            System.out.println("hello repeat: " + (System.currentTimeMillis() - start));
+            sleep5000();
+        }, 0, 2, TimeUnit.SECONDS);
+        executorService2.scheduleWithFixedDelay(() -> {
+            System.out.println("hello repeat, fixed start delay: " + (System.currentTimeMillis() - start));
+            sleep5000();
+        }, 0, 2, TimeUnit.SECONDS);
+
+        sleep20000();
     }
 
     @Test
@@ -76,11 +120,11 @@ public class ExecutorServiceTest {
                 }
         );
 
-        Future<String> future1 = threadPoolExecutor.submit(this::sleep);
-        Future<String> future2 = threadPoolExecutor.submit(this::sleep);
+        Future<String> future1 = threadPoolExecutor.submit(this::sleep1000);
+        Future<String> future2 = threadPoolExecutor.submit(this::sleep1000);
 
         try {
-            threadPoolExecutor.submit(this::sleep);
+            threadPoolExecutor.submit(this::sleep1000);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -97,10 +141,54 @@ public class ExecutorServiceTest {
         }
     }
 
-    private String sleep() {
+    private String sleep1000() {
         try {
             System.out.println(Thread.currentThread().getName());
             Thread.sleep(1000);
+            return Thread.currentThread().getName();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return Thread.currentThread().getName();
+        }
+    }
+
+    private String sleep3000() {
+        try {
+            System.out.println(Thread.currentThread().getName());
+            Thread.sleep(1000);
+            return Thread.currentThread().getName();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return Thread.currentThread().getName();
+        }
+    }
+
+    private String sleep5000() {
+        try {
+            System.out.println(Thread.currentThread().getName());
+            Thread.sleep(5000);
+            return Thread.currentThread().getName();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return Thread.currentThread().getName();
+        }
+    }
+
+    private String sleep10000() {
+        try {
+            System.out.println(Thread.currentThread().getName());
+            Thread.sleep(10000);
+            return Thread.currentThread().getName();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return Thread.currentThread().getName();
+        }
+    }
+
+    private String sleep20000() {
+        try {
+            System.out.println(Thread.currentThread().getName());
+            Thread.sleep(20000);
             return Thread.currentThread().getName();
         } catch (InterruptedException e) {
             e.printStackTrace();
