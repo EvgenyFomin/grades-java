5fe61a13a7ac971eb30f3024228b86743a3a76b0
fomin_e <fomin_e@vk.com>
2026-09-06 09:38:22 +0300

    master - add java 25

--- src/main/java/ru/protei/Main.java
+++ src/main/java/ru/protei/Main.java
@@ -1,9 +1,7 @@
 package ru.protei;
 
-import java.sql.SQLException;
-
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    void main() {
         System.out.println();
     }
 }
